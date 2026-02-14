5e133bd35d61cd66e24a13778480fc8fdc56b344
Adam Purnomo <adamsyammaszaki@gmail.com>
2026-02-14 18:19:20 +0900

    Add validation and derefenrece once taken

--- src/main/java/com/concurrency/queue/BlockingBoundedQueue.java
+++ src/main/java/com/concurrency/queue/BlockingBoundedQueue.java
@@ -9,14 +9,21 @@ public class BlockingBoundedQueue<T> {
 
 
     public BlockingBoundedQueue(int capacity) {
-        this.size = 0;
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity cannot be null");
+        }
         this.capacity = capacity;
         this.queue = (T[]) new Object[capacity];
         this.head = 0;
         this.tail = 0;
+        this.size = 0;
     }
 
     public void put(T object) throws InterruptedException {
+        if (object == null) {
+            throw new IllegalArgumentException("Inserted object cannot be null");
+        }
+
         synchronized (this) {
             while (size == capacity) {
                 this.wait(); // release the lock and acquire upon wakes up
@@ -37,6 +44,7 @@ public class BlockingBoundedQueue<T> {
                 this.wait();
             }
             object = queue[head];
+            queue[head] = null;
             head = (head + 1) % capacity;
             size -= 1;
             this.notifyAll();
